b03cda78b7be21aba1c54e450257a6c0c149a76a
visual_community@outlook.com <visual_community@outlook.com>
2019-08-17 08:49:16 -0400

    con MVC Completo

---
+++ MVCstudent/MVCstudent/Models/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace MVCstudent.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<MVCstudent.Models.Student> Students { get; set; }
+    }
+}
---
+++ MVCstudent/MVCstudent/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVCstudent.Models
+{
+    public class Student
+    {
+        [Key]
+        public int studentID { get; set; }
+        [Required]
+        [Display(Description ="Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Description ="First Name")]
+        public string FirstName { get; set; }
+
+    }
+}
